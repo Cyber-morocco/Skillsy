2fbc01b1eaed71f82967242c79b93569d05baf7d
safwane1080 <safwane.el.masaoudi@student.ehb.be>
2025-12-11 11:44:59 +0100

    week datumtabbs toevoegen met status

--- mobile/src/screens/Availability.tsx
+++ mobile/src/screens/Availability.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 const Availability: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<'week' | 'dates'>('week');
+
   return (
     <SafeAreaView>
-      <ScrollView />
+      <ScrollView>
+        <Text>Beschikbrheid</Text>
+        <Text> wanneer je beschikbaar bent</Text>
+
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity onPress={() => setActiveTab('week')}>
+            <Text>Per week</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setActiveTab('dates')}>
+            <Text>Specifieke datums</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
