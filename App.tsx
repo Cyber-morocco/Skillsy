55d0beb86616b1969ec09996404d7e7aac52ba8a
safwane1080 <safwane.el.masaoudi@student.ehb.be>
2025-12-11 11:36:59 +0100

    scherm structuur

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthStackNavigator from './mobile/src/navigation/AuthStack';
+import Availability from './mobile/src/screens/Availability';
 
 export default function App() {
-  return <AuthStackNavigator />;
+  return <Availability />;
 }
