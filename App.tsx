6d59cf4382809bd21c360deb4cb778810959ec83
Jouw Naam <ilies.mazouz@student.ehb.be>
2025-12-16 14:05:02 +0100

    app file fix

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import AuthStackNavigator from './mobile/src/navigation/AuthStack';
 import Availability from './mobile/src/screens/Availability';
 
